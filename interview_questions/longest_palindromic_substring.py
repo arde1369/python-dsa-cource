f521d4424f58125d4fb154e3ef0b01809d7fb40f
Arash Dehdari <arde1369@yahoo.com>
2026-01-19 01:19:58 -0600

    Adding more

---
+++ interview_questions/longest_palindromic_substring.py
@@ -0,0 +1,53 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+
+Solution approach:
+We treat every character in the string as a potential center of a palindrome. We expand outwards from this center to the left and right, checking if the characters match.
+
+Since palindromes can have an odd length (e.g., in "aba", the center is "b") or an even length (e.g., in "abba", the center is the gap between "bb"), 
+we must check both cases. For an odd length, l=r=i. For an even length, l=i and r=i+1.
+"""
+def isPalindrome(s,left,right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left-=1
+        right+=1
+    return s[left+1:right]
+
+def longestPalindrome(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    res = ""
+    for idx in range(len(s)):
+        even = isPalindrome(s, idx,idx+1)
+        odd = isPalindrome(s, idx, idx)
+        if len(res) < len(even):
+            res = even
+        if len(res) < len(odd):
+            res = odd
+    return res
+    
+
+s = "cbbd"
+expected = "bb"
+print("test case 1:")
+res = longestPalindrome(s)
+print(f"Input: {s} | Expected: {expected} | Actual: {res} | Results: ", "Pass" if res == expected else "Fail" )
+
+s = "babad"
+expected = "bab"
+print("test case 2:")
+res = longestPalindrome(s)
+print(f"Input: {s} | Expected: {expected} | Actual: {res} | Results: ", "Pass" if res == expected else "Fail" )
---
+++ interview_questions/reverse_integer.py
@@ -0,0 +1,62 @@
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+"""
+
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    #Convert to String
+    s = str(x)
+
+    #Check if numer was a negative
+    if s[0] == "-":
+        #If Negative Omit from from the beginning of the string
+        s= s[1:]
+
+        #Flip the string -> 's[::-1]' 
+        #Add negative sign back in -> '"-" +'
+        #Strip any leading zeros -> .lstrip("0")
+        s=("-" +s[::-1]).lstrip("0")
+    else:
+        #Flip the string -> 's[::-1]' 
+        #Strip any leading zeros -> .lstrip("0")
+        s = (s[::-1]).lstrip("0")
+    #Cast to int and return
+    return int(s)
+
+x = 120
+output = reverse(x)
+expected = 21
+print("Test case 1:")
+print(f"Input: {x} | Expected: {expected} | Actual: {output} | Results: ", "Pass" if output == expected else "Fail" )
+
+x = 123
+output = reverse(x)
+expected = 321
+print("Test case 2:")
+print(f"Input: {x} | Expected: {expected} | Actual: {output} | Results: ", "Pass" if output == expected else "Fail" )
+
+x = -123
+output = reverse(x)
+expected = -321
+print("Test case 3:")
+print(f"Input: {x} | Expected: {expected} | Actual: {output} | Results: ", "Pass" if output == expected else "Fail" )
